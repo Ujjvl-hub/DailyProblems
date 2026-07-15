3ac342bf44e1eabc4e2bc9873db4add0b860700a
Ujjwal <uk934010@gmail.com>
2026-07-15 12:25:59 +0530

    Time: 0 ms (100%), Space: 16.8 MB (70.36%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,63 +1,56 @@
 class Solution {
+    vector<int> x = {-1,1,0,0};
+    vector<int> y = {0,0,-1,1};
+private:
+    bool isValid(int i,int j, int n , int m){
+        if(i<0 || i>=n || j<0 || j>=m) return false;
+        return true;
+    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans =0;
 
-       vector<vector<bool>> visited(n, vector<bool>(m, false));
-        queue<pair<pair<int,int>, int>> q;   //(i,j),time
-       
-        //pushing all rotten oranges (source)
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    visited[i][j]= true;
+        queue<pair<int,int>> q;
+        int freshCount=0;
+
+        int time=0;
+
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==2) {
+                    q.push({i,j});
+                    grid[i][j]=0;
+                }
+                else if(grid[i][j]==1) freshCount++;
+            }
+        }
+
+        while(!q.empty() && freshCount>0){
+            time++;
+            int size = q.size();
+
+            while(size--){
+                pair<int,int> front = q.front();
+                q.pop();
+
+                int r = front.first;
+                int c = front.second;
+
+                for(int k= 0;k<4;k++){
+                    int row = r+ x[k];
+                    int col= c+ y[k];
+
+                    if(isValid(row,col,n,m) && grid[row][col]==1){
+                        q.push({row,col});
+                        grid[row][col]=0;
+                        freshCount--;
+                    }
                 }
             }
         }
-         
-        // bfs
-        while(!q.empty()){
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int time = q.front().second;
-            q.pop();
 
-            ans = max(ans,time);
-
-            //top
-            if(i-1>=0 && !visited[i-1][j] && grid[i-1][j]==1){
-                q.push({{i-1,j},time+1});
-                visited[i-1][j]= true;
-            }
-            //down
-            if(i+1<n && !visited[i+1][j] && grid[i+1][j]==1){
-                q.push({{i+1,j},time+1});
-                visited[i+1][j]= true;
-            }
-
-            // left
-            if(j-1>=0 && !visited[i][j-1] && grid[i][j-1]==1){
-                q.push({{i,j-1},time+1});
-                visited[i][j-1]= true;
-            }
-            //right
-            if(j+1<m && !visited[i][j+1] && grid[i][j+1]==1){
-                q.push({{i,j+1},time+1});
-                visited[i][j+1]= true;
-            }
-
-        }
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    return -1;
-                }
-            }
-        }
-        return ans;
+        if(freshCount>0) return -1;
+        return time;
     }
 };
