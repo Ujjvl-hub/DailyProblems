14976b693acaba35279647bec717430b1d1787c3
Ujjwal <uk934010@gmail.com>
2026-07-21 17:50:46 +0530

    Time: 0 ms (100%), Space: 28.4 MB (50.77%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,25 +1,23 @@
 class Solution {
-    // x and y coordinate for up down left and right
     vector<int> x = {-1,1,0,0};
     vector<int> y = {0,0,-1,1};
 
 private:
-    bool isValid(int i,int j, int n, int m){
+    bool isValid(int i,int j,int n,int m){
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
     }
 
-    void dfs(int i,int j,int n, int m,vector<vector<int>>& grid, vector<vector<bool>> &visited,int &sum ){
-        visited[i][j]= true;
-        sum+=grid[i][j];
+    void dfs(int i,int j,int n,int m,vector<vector<bool>> &visited,vector<vector<int>>& grid, int& count){
+        visited[i][j] = true;
 
-        // find new row n col
-        for(int k=0 ; k<4 ; k++){
+        for(int k=0;k<4;k++){
             int r = i + x[k];
             int c = j + y[k];
 
-            if(isValid(r,c,n,m) && !visited[r][c] && grid[r][c]!=0){
-                dfs(r,c,n,m,grid,visited,sum);
+            if(isValid(r,c,n,m) && !visited[r][c] && grid[r][c]==1){
+                count++;
+                dfs(r,c,n,m,visited,grid,count);
             }
         }
     }
@@ -28,19 +26,21 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        int ans=0;
-
+        int area =0;
         vector<vector<bool>> visited(n,vector<bool>(m,false));
-
+        int count=0;
         for(int i=0;i<n;i++){
+           
             for(int j=0;j<m;j++){
-                if(!visited[i][j] && grid[i][j]!=0){
-                    int sum=0;
-                    dfs(i,j,n,m,grid,visited,sum);
-                    ans = max(ans,sum);
+                if(isValid(i,j,n,m) && !visited[i][j] && grid[i][j]==1){
+                    count++;
+                    dfs(i,j,n,m,visited,grid,count);
+                    area = max(area,count);
+                    count=0;
                 }
             }
+
         }
-        return ans;
+        return area;
     }
 };
