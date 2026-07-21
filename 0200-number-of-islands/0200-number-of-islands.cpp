99ea5ef6875de045d66fb2832318835d73355e84
Ujjwal <uk934010@gmail.com>
2026-07-21 17:29:22 +0530

    Time: 25 ms (72.3%), Space: 16.8 MB (57.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,13 +3,13 @@ class Solution {
     vector<int> y = {0,0,-1,1};
 
 private:
-    bool isValid(int i, int j, int n ,int m){
+    bool isValid(int i,int j, int n , int m){
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
     }
 
-    void dfs(int i,int j, int n,int m ,vector<vector<bool>> &visited,vector<vector<char>>& grid){
-        visited[i][j]= true;
+    void dfs(int i,int j,int n,int m, vector<vector<bool>> &visited, vector<vector<char>> &grid){
+        visited[i][j] = true;
 
         for(int k=0;k<4;k++){
             int r = i + x[k];
@@ -22,19 +22,17 @@ private:
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
-
         int n = grid.size();
         int m = grid[0].size();
+        int count =0;
 
-        int count=0;
         vector<vector<bool>> visited(n,vector<bool>(m,false));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !visited[i][j]){
-                    count++;
+                if(isValid(i,j,n,m) && !visited[i][j] && grid[i][j]=='1'){
                     dfs(i,j,n,m,visited,grid);
-                    
+                    count++;
                 }
             }
         }
