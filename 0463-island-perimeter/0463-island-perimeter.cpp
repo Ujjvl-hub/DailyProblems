0f31cfa10ce8a6920a1dcd86ea15aa2e16debb49
Ujjwal <uk934010@gmail.com>
2026-07-21 18:18:39 +0530

    Time: 36 ms (6.09%), Space: 105.2 MB (21.45%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    vector<int> x = {-1,1,0,0};
+    vector<int> y = {0,0,-1,1};
+
+private:
+    bool isValid(int i,int j, int n,int m){
+        if(i<0 || i>=n || j<0 || j>=m) return false;
+        return true;
+    }
+
+    void dfs(int i,int j,int n,int m, vector<vector<bool>> &visited, vector<vector<int>> &grid, int& count){
+        visited[i][j] = true;
+        count+=4;
+        for(int k=0;k<4;k++){
+            int r = i + x[k];
+            int c = j + y[k];
+
+            if(isValid(r,c,n,m) && grid[r][c]==1){
+                count-=1;
+                if(!visited[r][c]){
+                    dfs(r,c,n,m,visited,grid,count);
+                }
+                
+            }
+        }
+    }
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int ans =0;
+        int count=0;
+
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(isValid(i,j,n,m) && !visited[i][j] && grid[i][j]==1){
+                    dfs(i,j,n,m,visited,grid,count);
+                    ans = max(ans,count);
+                    count=0;
+                }
+            }
+        }
+        return ans;
+    }
+};
