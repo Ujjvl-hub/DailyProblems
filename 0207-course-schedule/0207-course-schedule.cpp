5cdd6e00c990404aff9b70ee0fa83e711d2d6f2b
Ujjwal <uk934010@gmail.com>
2026-07-22 23:12:40 +0530

    Time: 11 ms (20.59%), Space: 21.9 MB (5.06%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,40 @@
 class Solution {
+    private:
+    bool dfs(int node,unordered_map<int,bool> &visited,unordered_map<int,bool> &dfsCycle, unordered_map<int,vector<int>> &adj){
+        visited[node]= true;
+        dfsCycle[node] = true;
+
+        for(auto nbr : adj[node]){
+            if(visited[nbr] && dfsCycle[nbr]){
+                return true;
+            }else if(!visited[nbr]){
+                bool ans = dfs(nbr,visited,dfsCycle,adj);
+                if(ans) return true;
+            }
+        }
+        dfsCycle[node] = false;
+        return false;
+    }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         unordered_map<int,vector<int>> adj;
+
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
 
-            adj[u].push_back(v);
-
-        }
-        vector<int> indegree(numCourses,0);
-        for(auto i: adj){
-            for(auto j: i.second){
-                indegree[j]++;
-            }
+            adj[v].push_back(u);
         }
 
-        int count=0;
-        queue<int> q;
+        unordered_map<int,bool> visited;
+        unordered_map<int,bool> dfsCycle;
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0) q.push(i);
-        }
-
-        while(!q.empty()){
-            int front = q.front();
-            q.pop();
-            count++;
-            for(auto i: adj[front]){
-                indegree[i]--;
-                if(indegree[i]==0){
-                    q.push(i);
-                }
+            if(!visited[i]){
+               bool res =  dfs(i,visited,dfsCycle,adj);
+               if(res) return false;
             }
-
         }
-       if(count==numCourses) return true;
-       else return false;
-        
+
+    return true;
     }
 };
