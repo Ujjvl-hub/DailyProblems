b03369488b506444b61b08fe983e9fe1947c5c08
Ujjwal <uk934010@gmail.com>
2026-07-26 13:30:20 +0530

    Time: 12 ms (33.46%), Space: 14.7 MB (33.78%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,66 +1,56 @@
 class Solution {
     vector<int> x = {-1,1,0,0};
     vector<int> y = {0,0,-1,1};
-private:
 
+private:
     bool isValid(int i,int j, int n ,int m){
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
-    }
-    bool bfs(vector<vector<int>>& grid, int n,int m ,int mid){
-        queue<pair<int,int>> q;
-        vector<vector<bool>> visited(n,vector<bool>(m,false));
-
-        q.push({0,0});
-        visited[0][0] = true;
-
-        while(!q.empty()){
-            auto top = q.front();
-            q.pop();
-
-            int r = top.first;
-            int c = top.second;
-
-            if(r==n-1 && c==m-1) return true;
-            for(int k=0;k<4;k++){
-                int row = r + x[k];
-                int col = c + y[k];
-
-                if(isValid(row,col,n,m) && !visited[row][col] && mid>=grid[row][col]){
-                    q.push({row,col});
-                    visited[row][col] = true;
-                }
-            }
-        }
-        return false;
     }
 public:
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
 
-        int low= grid[0][0];
-        int high = grid[0][0];
+        vector<vector<int>> distance(n,vector<int>(m,INT_MAX));
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                high = max(high,grid[i][j]);
+        set<pair<int,pair<int,int>>> st;
+
+        distance[0][0] = grid[0][0];
+        int money = grid[0][0];
+        st.insert({money,{0,0}});  // {money, {row,col}}
+
+
+        while(!st.empty()){
+            auto top = *(st.begin());
+            st.erase(st.begin());
+            int minCost = top.first;
+
+            int row = top.second.first;
+            int col = top.second.second;
+
+            if(minCost > distance[row][col]){
+                continue;
+            }
+
+            for(int k=0;k<4;k++){
+                int r = row + x[k];
+                int c = col + y[k];
+
+                if(isValid(r,c,n,m)) {
+                    int newMoney  = max(minCost, grid[r][c]);
+                    if( newMoney < distance[r][c]){
+                        auto record = st.find({newMoney,{r,c}});
+                        if(record!=st.end()) st.erase(record);
+                        distance[r][c] = newMoney;
+                        st.insert({newMoney,{r,c}});
+
+                    }
+                }
+                
             }
         }
+        return distance[n-1][m-1];
 
-        int res =0;
-
-
-        while(low<=high){
-            int mid = low+ (high-low)/2;
-            if(bfs(grid,n,m,mid)){
-                res= mid;
-                high = mid-1;
-            }else{
-                low = mid+1;
-            }
-        }
-    return res;
-        
     }
 };
