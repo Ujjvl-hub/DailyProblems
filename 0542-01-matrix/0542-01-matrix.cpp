f95897a913f80eebbaa4b435b7ef71e9c9bc821d
Ujjwal <uk934010@gmail.com>
2026-08-11 23:23:28 +0530

    Time: 22 ms (43.41%), Space: 36.9 MB (53.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,7 +2,7 @@ class Solution {
     vector<int> x = {-1,1,0,0};
     vector<int> y = {0,0,-1,1};
 private:
-    bool isValid(int i,int j,int n, int m){
+    bool isValid(int i, int j, int n, int m){
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
     }
@@ -12,11 +12,13 @@ public:
         int m = mat[0].size();
 
         vector<vector<int>> res(n,vector<int>(m,-1));
+        vector<vector<bool>> visited(n,vector<bool>(m,false));
+
         queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                
-                if(mat[i][j]==0) {
+                if(mat[i][j]==0){
+                    visited[i][j] = true;
                     q.push({i,j});
                     res[i][j] = 0;
                 }
@@ -24,22 +26,22 @@ public:
         }
 
         while(!q.empty()){
-            auto temp = q.front();
+            auto p = q.front();
             q.pop();
 
-            int row = temp.first;
-            int col = temp.second;
+            int row = p.first;
+            int col = p.second;
 
             for(int k=0;k<4;k++){
                 int r = row + x[k];
                 int c = col + y[k];
 
-                if(isValid(r,c,n,m) && res[r][c]==-1){
-                    res[r][c] = res[row][col]+1;
-                    q.push({r,c});
+                if(isValid(r,c,n,m) && !visited[r][c]){
+                    visited[r][c] = true;
+                   res[r][c] = res[row][col] +1;
+                    q.push({r,c});   
                 }
             }
-
         }
         return res;
     }
