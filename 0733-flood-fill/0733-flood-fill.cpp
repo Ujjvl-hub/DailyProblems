cfe94fa846f8cb386ad6143fbcd102d80011d089
Ujjwal <uk934010@gmail.com>
2026-08-11 22:27:32 +0530

    Time: 0 ms (100%), Space: 18.1 MB (75.55%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,23 +1,23 @@
 class Solution {
     vector<int> x = {-1,1,0,0};
     vector<int> y = {0,0,-1,1};
-    private:
+private:
     bool isValid(int i,int j, int n ,int m){
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
     }
 
-
-    void dfs(int r, int c,int n,int m, vector<vector<bool>> &visited,vector<vector<int>>& image,int color, int initialColor){
-        visited[r][c]= true;
-        image[r][c] = color;
+    void dfs(int i,int j, int color,vector<vector<int>>& image,vector<vector<bool>>& visited,int n,int m, int originalCol){
+        visited[i][j] = true;
+        image[i][j] = color;
+        
 
         for(int k=0;k<4;k++){
-            int row = r + x[k];
-            int col = c + y[k];
+            int r = i + x[k];
+            int c = j +y[k];
 
-            if(isValid(row,col,n,m) && !visited[row][col] && image[row][col]==initialColor){
-                dfs(row,col,n,m,visited,image,color,initialColor);
+            if(isValid(r,c,n,m) && !visited[r][c] && image[r][c]==originalCol){
+                dfs(r,c,color,image,visited,n,m,originalCol);
             }
         }
     }
@@ -27,8 +27,9 @@ public:
         int m = image[0].size();
 
         vector<vector<bool>> visited(n,vector<bool>(m,false));
-        int initialColor = image[sr][sc];
-        dfs(sr,sc,n,m,visited,image,color,initialColor);
+        int originalCol = image[sr][sc];
+
+        dfs(sr,sc,color,image,visited, n,m,originalCol);
         return image;
     }
 };
