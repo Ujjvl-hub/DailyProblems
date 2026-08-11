64d92841b0eae4558f068af283f91437ab23606b
Ujjwal <uk934010@gmail.com>
2026-08-11 22:44:55 +0530

    Time: 3 ms (75.25%), Space: 19.2 MB (81.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,14 @@
 class Solution {
-    private:
-    bool dfs(int node,unordered_map<int,bool> &visited,unordered_map<int,bool> &dfsCycle, unordered_map<int,vector<int>> &adj){
-        visited[node]= true;
+private:
+    bool isCycle(int node, vector<bool> &visited,vector<bool> &dfsCycle, vector<vector<int>> &adj){
+        visited[node] = true;
         dfsCycle[node] = true;
 
-        for(auto nbr : adj[node]){
-            if(visited[nbr] && dfsCycle[nbr]){
-                return true;
-            }else if(!visited[nbr]){
-                bool ans = dfs(nbr,visited,dfsCycle,adj);
-                if(ans) return true;
+        for(auto i: adj[node]){
+            if(visited[i] && dfsCycle[i]) return true;
+            else if(!visited[i]){
+                bool res = isCycle(i,visited,dfsCycle,adj);
+                if(res) return true;
             }
         }
         dfsCycle[node] = false;
@@ -17,8 +16,7 @@ class Solution {
     }
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>> adj;
-
+        vector<vector<int>> adj(numCourses);
         for(int i=0;i<prerequisites.size();i++){
             int u = prerequisites[i][0];
             int v = prerequisites[i][1];
@@ -26,15 +24,15 @@ public:
             adj[v].push_back(u);
         }
 
-        unordered_map<int,bool> visited;
-        unordered_map<int,bool> dfsCycle;
+        vector<bool> visited(numCourses, false);
+        vector<bool> dfsCycle(numCourses, false);
+
         for(int i=0;i<numCourses;i++){
             if(!visited[i]){
-               bool res =  dfs(i,visited,dfsCycle,adj);
-               if(res) return false;
+                bool res = isCycle(i,visited,dfsCycle,adj);
+                if(res) return false;
             }
         }
-
-    return true;
+        return true;
     }
 };
