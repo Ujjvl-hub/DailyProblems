811aae09775cf6e6b56caffdae56af51a8b43e98
Ujjwal <uk934010@gmail.com>
2026-08-17 09:23:18 +0530

    Time: 35 ms (45.04%), Space: 88.4 MB (83.16%) - LeetHub

---
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    bool isPossible(int mid, vector<int> &quantities, int n){
+        int count=0;
+        for(int i=0;i<quantities.size();i++){
+            count+=(quantities[i]/mid);
+            if(quantities[i]%mid!=0) count++;
+            if(count>n){
+                return false;
+            }
+        }  
+        return true;
+    }
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int low=1;
+        int high = 0;
+        int ans  =0;
+        for(int i=0;i<quantities.size();i++){
+            high = max(high,quantities[i]);
+        }
+
+        while(low<=high){
+            int mid= low+(high-low)/2;
+
+            if(isPossible(mid,quantities,n)){
+                ans = mid;
+                high = mid-1;
+
+            }else low = mid+1;
+        }
+        return ans;
+    }
+};
