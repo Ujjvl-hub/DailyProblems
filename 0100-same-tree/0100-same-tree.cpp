6630b421eab01149481bdafac7c963a73da3629a
Ujjwal <uk934010@gmail.com>
2026-08-29 01:01:32 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.71%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,11 +15,10 @@ public:
         if(p==NULL && q==NULL) return true;
         if(p==NULL || q==NULL) return false;
         if(p->val != q->val) return false;
-        
-        bool left = isSameTree(p->left,q->left);
-        bool right = isSameTree(p->right,q->right);
 
-        if(left && right ) return true;
-       return false;
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+
+        return left && right;
     }
 };
