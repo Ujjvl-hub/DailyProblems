eca9c3f44e0de495e5a36720536b40e43dab9172
Ujjwal <uk934010@gmail.com>
2026-08-29 23:27:13 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.59%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -10,30 +10,43 @@
  * };
  */
 class Solution {
-int getHeight(TreeNode* root){
-    if(root==NULL) return 0;
-    int left = getHeight(root->left);
-    int right = getHeight(root->right);
-    return max(left,right)+1;
-}
-int solve(TreeNode* root){
-    if(root==NULL) return 1;
-    int left = getHeight(root->left);
-    int right = getHeight(root->right);
-    if(abs(left-right)>1) return -1;
+private:
+// int getHeight(TreeNode* root){
+//     if(root==NULL) return 0;
+//     int left = getHeight(root->left);
+//     int right = getHeight(root->right);
+//     return max(left,right)+1;
+// }
+// int solve(TreeNode* root){
+//     if(root==NULL) return 1;
+//     int left = getHeight(root->left);
+//     int right = getHeight(root->right);
+//     if(abs(left-right)>1) return -1;
 
-    int l = solve(root->left);
-    int r = solve(root->right);
+//     int l = solve(root->left);
+//     int r = solve(root->right);
 
-    if(l==1 && r==1) return 1;
-    return -1;
-}
+//     if(l==1 && r==1) return 1;
+//     return -1;
+// }
+    pair<bool,int> solve(TreeNode* root){
+        if(root==NULL) return {true,0};
+        pair<bool,int> left = solve(root->left);
+        pair<bool,int> right = solve(root->right); 
+        int height = max(left.second,right.second)+1;
+
+        pair<bool,int> res;
+        if(left.first && right.first && abs(left.second-right.second)<=1){
+            res.first= true;
+        }else{
+            res.first = false;
+        }
+        res.second = height;
+        return res;
+    }
 public:
     bool isBalanced(TreeNode* root) {
         if(root==NULL) return true;
-        if(solve(root)==1){
-            return true;
-        }
-        return false;
+        return solve(root).first;
     }
 };
