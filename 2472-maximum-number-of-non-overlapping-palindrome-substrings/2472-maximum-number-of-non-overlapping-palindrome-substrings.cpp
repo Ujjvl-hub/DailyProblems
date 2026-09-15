dc55cf8db3484a16f3c8d77a5d53c66eefa9b1fa
Ujjwal <uk934010@gmail.com>
2026-09-15 09:56:24 +0530

    Time: 823 ms (10.47%), Space: 216.5 MB (13.87%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -13,11 +13,26 @@ public:
         int n = s.length();
 
         if(k==1) return n;
+
+        vector<vector<bool>> isPalindrome(n,vector<bool>(n,false));
+
+        for(int L =1;L<=n;L++){
+            for(int i=0;i+L<=n;i++){
+                int j = i+L-1;
+                if(i==j){
+                    isPalindrome[i][j] = true;
+                }else if(i+1==j){
+                    isPalindrome[i][j] = s[i]==s[j];
+                }else{
+                    isPalindrome[i][j] = ((s[i]==s[j]) && isPalindrome[i+1][j-1]==true);
+                }
+            }
+        }
         vector<vector<int>> dp(n+1,vector<int>(n+1));
 
         for(int i=n-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
-                if(isPalindrome(i,j,s)){
+                if(isPalindrome[i][j]){
                     int take = 1+ (j+k<=n ?dp[j+1][j+k]:0);
                     int grow = dp[i][j+1];
                     int slide = dp[i+1][j+1];
